ccb90d4ce9189236ba9f8ac1117511a57bcb97d7
stvking <tgfbikes@gmail.com>
2016-07-25 11:38:49 -0600

    initial app set up

---
+++ todo/server.js
@@ -0,0 +1,23 @@
+'use strict';
+
+var express = require('express');
+var path = require('path');
+var colors = require('colors');
+var morgan = require('morgan');
+var skipper = require('skipper');
+
+var app = express();
+
+//app.use(express.static(__dirname + '/dist'));
+
+app.use(morgan('dev'));
+//app.use(passport.initialize())
+app.use(skipper());
+
+app.get('*', function (req, res) {
+    res.sendFile(path.join(__dirname, '/index.html'));
+});
+
+app.listen(9005, function () {
+	  console.log('Listening on localhost:9005'.rainbow);
+});
